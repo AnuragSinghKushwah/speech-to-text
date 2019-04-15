c48967849235451a6887e48e3848358c992ae62f
Anurag <anurag786kushwah@gmail.com>
2019-04-15 14:03:37 +0530

    Added Google Text to Speech with output mp3 as well

--- gtts_demo.py
+++ gtts_demo.py
@@ -1,5 +1,11 @@
 from gtts import gTTS
 import os
+
+# Calling Speech to Text Engine
 tts = gTTS(text='Good morning', lang='en')
+
+# Saving To MP3
 tts.save("good.mp3")
+
+#Using mpg321 to speak out the output file
 os.system("mpg321 good.mp3")
